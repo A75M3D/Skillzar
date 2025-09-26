9632d2ca1b2807d30cf236a94a15a2720aeaebf4
A7M <ahmedmoha0med11@gmail.com>
2025-09-26 22:27:17 +0200

    Update sw.js
    D

--- sw.js
+++ sw.js
@@ -1,8 +1,1 @@
-// sw.js - Basic Service Worker
-self.addEventListener('install', function(event) {
-  console.log('Service Worker installed');
-});
 
-self.addEventListener('fetch', function(event) {
-  // Basic fetch handler
-});
